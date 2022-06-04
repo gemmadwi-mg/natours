4d7dfa599f47ef1a61c6ecddf97d1fbd4ab6e6f9
GDwiP <gemma.19051@mhs.unesa.ac.id.com>
2022-06-04 09:47:47 +0700

    Implemented CORS

--- app.js
+++ app.js
@@ -29,13 +29,15 @@ app.set('views', path.join(__dirname, 'views'));
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-const corsOptions = {
-  origin: 'http://localhost:3000',
-  credentials: true,            //access-control-allow-credentials:true
-  optionSuccessStatus: 200
-}
+// const corsOptions = {
+//   origin: 'http://localhost:3000',
+//   credentials: true,            //access-control-allow-credentials:true
+//   optionSuccessStatus: 200
+// }
 
-app.use(cors(corsOptions));
+app.use(cors());
+
+app.options('*', cors());
 
 // Set security HTTP headers
 app.use(helmet());
